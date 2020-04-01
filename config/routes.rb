597a237fcec63c46bef171b9259e69d8a56e0db7
NanZ-hash <afnanxmt@gmail.com>
2020-04-01 15:50:37 +0300

    saving files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  
+  get "bootstrap", to: "artists#bootstrap"
+
+  root 'artists#index'
+  resources :artists , :songs
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
